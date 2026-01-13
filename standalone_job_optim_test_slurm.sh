542a3932318931ace7243ce8562aa802a76767b0
rsaldanha2019 <rsaldanha554@gmail.com>
2026-01-13 08:54:00 +0530

    Update standalone_job_optim_test_slurm.sh

--- standalone_job_optim_test_slurm.sh
+++ standalone_job_optim_test_slurm.sh
@@ -95,7 +95,7 @@ fi
 # --- RUN ---
 if [ "$ENV_TYPE" == "conda" ]; then
     echo "Running inside conda env: $ENV_VALUE"
-    bash -c "conda run -n \"$ENV_VALUE\" bash -c 'export MASTER_PORT=$MASTER_PORT && python -u -m torch.distributed.run --nproc-per-node=$PPN --master-port=$MASTER_PORT -m lang_ident_classifier.cli.hyperparam_selection_model_optim --config=$CONFIG_FILE $CPU_ARG --backend=$BACKEND --run_timestamp=$RUN_TIMESTAMP $RESUME_ARG >> \"$JOB_LOG_DIR/RUN_$RUN_TIMESTAMP.out\" 2>&1'"
+    bash -c "conda run -n \"$ENV_VALUE\" bash -c 'export MASTER_PORT=$MASTER_PORT && python -u -m torch.distributed.run --nproc-per-node=$PPN --master-port=$MASTER_PORT -m lang_ident_classifier.cli.lang_ident_classifier_api --config=$CONFIG_FILE $CPU_ARG --backend=$BACKEND --run_timestamp=$RUN_TIMESTAMP $RESUME_ARG >> \"$JOB_LOG_DIR/RUN_$RUN_TIMESTAMP.out\" 2>&1'"
 elif [ "$ENV_TYPE" == "docker" ]; then
     echo "Running inside Docker image: $ENV_VALUE"
     MY_UID=$(id -u)
@@ -124,7 +124,7 @@ elif [ "$ENV_TYPE" == "docker" ]; then
         python -u -m torch.distributed.run \
             --nproc-per-node $PPN \
             --master-port $MASTER_PORT \
-            -m lang_ident_classifier.cli.hyperparam_selection_model_optim \
+            -m lang_ident_classifier.cli.lang_ident_classifier_api \
             --config "/app/$CONFIG_FILE" \
             $CPU_ARG \
             --backend $BACKEND \
@@ -136,7 +136,7 @@ elif [ "$ENV_TYPE" == "none" ]; then
     python -u -m torch.distributed.run \
         --nproc-per-node $PPN \
         --master-port $MASTER_PORT \
-        -m lang_ident_classifier.cli.hyperparam_selection_model_optim \
+        -m lang_ident_classifier.cli.lang_ident_classifier_api \
         --config $CONFIG_FILE \
         $CPU_ARG \
         --backend $BACKEND \
